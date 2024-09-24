13a34636656464325a2f03052ce23be67af237ac
mewshh <giorgi25k@gmail.com>
2024-09-24 21:08:35 +0400

    #Added player idle, run animations, player changes rotation based on where its walking

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,8 +3,12 @@ using Zenject;
 
 public class PlayerController : MonoBehaviour
 {
-    public float movementSpeed = 5f;
+    [SerializeField] private float _movementSpeed = 5f;
+    [SerializeField] private Transform _playerVisual;
+    [SerializeField] private Animator _animator;
     private UIManager _uiManager;
+
+    private static string RUNNING_ANIMATION = "Running";
 
     [Inject]
     public void Construct(UIManager uiManager)
@@ -27,11 +31,23 @@ public class PlayerController : MonoBehaviour
     {
         Vector2 direction = _uiManager.JoystickDirection;
 
+        _animator.SetBool(RUNNING_ANIMATION, true);
+
+        HandlePlayerRotation(direction);
+
         if (direction == Vector2.zero)
         {
+            _animator.SetBool(RUNNING_ANIMATION, false);
             return;
         }
 
-        transform.Translate(direction * movementSpeed * Time.deltaTime);
+
+        transform.Translate(direction * _movementSpeed * Time.deltaTime);
+    }
+
+    private void HandlePlayerRotation(Vector2 direction)
+    {
+        if (direction == Vector2.zero) return;
+        _playerVisual.localScale = new Vector3((direction.x > 0 ? 1 : -1), 1, 1);
     }
 }
